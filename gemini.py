6060455d97d6cfd211d503c7ec177519452aef2b
anon <122219518+avocado-beans@users.noreply.github.com>
2024-06-09 09:48:28 +0300

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,11 +1,11 @@
 import google.generativeai as genai
 import time
 
-def answer(key, prompt, image):
+def answer(key, prompt, image, model_name):
     genai.configure(api_key=key)
     sample_file = genai.upload_file(path=image, display_name="Question")
     file = genai.get_file(name=sample_file.name)
-    model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
+    model = genai.GenerativeModel(model_name=model_name)
     generation_config = genai.GenerationConfig(temperature=0.0)
     response = model.generate_content([prompt, sample_file], generation_config=generation_config)
     return(response.text)
