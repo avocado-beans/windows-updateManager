6e4f2f804514b4b212970b054f016e5ff1024828
anon <122219518+avocado-beans@users.noreply.github.com>
2024-06-28 17:23:43 +0200

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import time
 
-def answer(key, prompt, image, model_name):
+def clean_gemini(key, prompt, model_name):
     genai.configure(api_key=key)
     model = genai.GenerativeModel(model_name=model_name)
     generation_config = genai.GenerationConfig(temperature=0.0)
