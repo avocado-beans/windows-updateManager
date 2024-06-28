225b69d714f334147908fb0f8d1a0922e4301cb7
anon <122219518+avocado-beans@users.noreply.github.com>
2024-06-28 18:33:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def confirm(request: Request):
     
 @app.post("/")
 async def read_items(request: Request):
-    print(request.json())
+    print(await request.json())
     user = await request.json()['id']
     print(f'from {user}')
 
